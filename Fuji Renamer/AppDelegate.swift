4a94a20d73a479932348bc4a9e24ea55a0ed6d0a
Koray Birand <kbirand@mac.com>
2020-06-21 15:33:11 +0300

    Initial Commit

---
+++ Fuji Renamer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fuji Renamer
+//
+//  Created by Koray Birand on 21.06.2020.
+//  Copyright © 2020 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Fuji Renamer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fuji Renamer
+//
+//  Created by Koray Birand on 21.06.2020.
+//  Copyright © 2020 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
